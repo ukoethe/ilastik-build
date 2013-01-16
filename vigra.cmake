57aff0d6f142eae6f32aa51ac27927702d9b645c
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2013-01-16 01:01:20 +0100

    added vigra

---
+++ vigra.cmake
@@ -0,0 +1,49 @@
+#
+# Install vigra libraries from source
+#
+
+if (NOT vigra_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+
+include (jpeg)
+include (tiff)
+include (libpng)
+include (fftw)
+include (hdf5)
+include (python)
+include (boost)
+include (numpy)
+
+external_git_repo (vigra
+    HEAD
+    # http://github.com/ukoethe/vigra.git)
+    hci:repos/vigra)
+    
+message ("Installing ${vigra_NAME} into ilastik build area: ${ILASTIK_DEPENDENCY_DIR} ...")
+ExternalProject_Add(${vigra_NAME}
+    DEPENDS             ${jpeg_NAME} ${tiff_NAME} ${libpng_NAME} ${fftw_NAME}
+                        ${hdf5_NAME} ${python_NAME} ${boost_NAME} # ${numpy_NAME} 
+    PREFIX              ${ILASTIK_DEPENDENCY_DIR}
+    GIT_REPOSITORY      ${vigra_URL}
+    UPDATE_COMMAND      ""
+    PATCH_COMMAND       ""
+    CONFIGURE_COMMAND   ${CMAKE_COMMAND} ${vigra_SRC_DIR} 
+        -G ${CMAKE_GENERATOR} 
+        -DCMAKE_INSTALL_PREFIX=${ILASTIK_DEPENDENCY_DIR}
+        -DDEPENDENCY_SEARCH_PREFIX=${ILASTIK_DEPENDENCY_DIR}
+        -DPYTHON_EXECUTABLE=${PYTHON_EXE}
+        -DHDF5_CPPFLAGS=-D_HDF5USEDLL_
+    BUILD_COMMAND       devenv vigra.sln /build Release /project vigraimpex
+                        \ndevenv vigra.sln /build Release /project vigranumpy
+    TEST_COMMAND        "" # devenv vigra.sln /build Release /project check
+    INSTALL_COMMAND     devenv vigra.sln /build Release /project INSTALL
+)
+
+set_target_properties(${vigra_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
+
+endif (NOT vigra_NAME)
+
