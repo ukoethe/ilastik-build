b999f62c514ff347c242c664ae11a691e93805ca
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-04-18 15:26:02 +0200

    add scikit-image scripts

---
+++ scikit_image.cmake
@@ -0,0 +1,44 @@
+#
+# Install scikit__image library from source
+#
+
+if (NOT scikit_image_NAME)
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
+
+include (ExternalProject)
+include (ExternalSource)
+
+include (python)
+include (python_packages)
+include (numpy)
+include (scipy)
+
+external_source (scikit_image
+    0.8.2
+    scikit-image-0.8.2.tar.gz
+    9f5184c43f9fc1e9f43feea161b9f081
+    http://pypi.python.org/packages/source/s/scikit-image
+    FORCE)
+
+configure_file(build_scikit_image.bat.in ${ILASTIK_DEPENDENCY_DIR}/tmp/build_scikit_image.bat)
+file(TO_NATIVE_PATH ${ILASTIK_DEPENDENCY_DIR}/tmp/build_scikit__image.bat SCIKIT_IMAGE_BUILD_BAT)
+
+message ("Installing ${scikit_image_NAME} into ilastik build area: ${ILASTIK_DEPENDENCY_DIR} ...")
+ExternalProject_Add(${scikit_image_NAME}
+    DEPENDS             ${python_NAME} ${cython_NAME} ${numpy_NAME} ${scipy_NAME} 
+    PREFIX              ${ILASTIK_DEPENDENCY_DIR}
+    URL                 ${scikit_image_URL}
+    URL_MD5             ${scikit_image_MD5}
+    UPDATE_COMMAND      ""
+    PATCH_COMMAND       ""
+    CONFIGURE_COMMAND   ""
+    BUILD_COMMAND       ${SCIKIT_IMAGE_BUILD_BAT}
+    BUILD_IN_SOURCE     1
+    TEST_COMMAND        ""
+    INSTALL_COMMAND     ""
+)
+
+set_target_properties(${scikit_image_NAME} PROPERTIES EXCLUDE_FROM_ALL ON)
+
+endif (NOT scikit_image_NAME)
