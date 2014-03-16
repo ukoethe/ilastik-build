33bdb73a1e506492002d3853034b1a061f3346a2
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2014-03-16 19:36:53 +0100

    improved the fix of h5py

--- patches/patch_h5py.py
+++ patches/patch_h5py.py
@@ -7,7 +7,7 @@ s = open(filename).read()
 s = re.sub(r'hdf5dll18', 'hdf5', s)
 s = re.sub(r'hdf5_hldll', 'hdf5_hl', s)
 
-# the following define apparently fixes a crash in lzf compression
-s = re.sub(r"'define_macros'\s*:\s*\[", "'define_macros' : [('INIT_HTAB', 1), ", s)
+# the following define is required for lzf compression on Win64
+s = re.sub(r"\('_HDF5USEDLL_',\s*None\)", "('_HDF5USEDLL_', None), ('WIN32', 1)", s)
 
 open(filename, "w").write(s)
